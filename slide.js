ee7ba1bc4429fa6513f035a97bc5cb7ee8a36fc7
ashashlin <104874547+ashashlin@users.noreply.github.com>
2025-01-23 05:57:03 +0800

    finish basic css for image modal

---
+++ slide.js
@@ -0,0 +1,6 @@
+const viewImage = document.querySelector(".js-view-img");
+const imageModal = document.querySelector(".image-modal");
+
+viewImage.addEventListener("click", () => {
+  imageModal.showModal();
+});
