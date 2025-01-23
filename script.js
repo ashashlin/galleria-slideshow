2a6b30ca9653ce0962423b83e941d8c0ded6b172
ashashlin <104874547+ashashlin@users.noreply.github.com>
2025-01-24 01:12:15 +0800

    navigate to slide when clicking on a slide or start slideshow

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ function generateSlidesHTML() {
     const artistName = painting.artist.name;
 
     slidesHTML += `
-      <div class="slide">
+      <div class="slide js-slide"
+      data-name="${name}">
         <img
           class="painting-thumbnail"
           src="${thumbnail}"
@@ -28,3 +29,26 @@ function generateSlidesHTML() {
   slides.innerHTML = slidesHTML;
 }
 generateSlidesHTML();
+
+function navigateToASlide() {
+  const allSlides = document.querySelectorAll(".js-slide");
+
+  allSlides.forEach((slide) => {
+    slide.addEventListener("click", () => {
+      const { name } = slide.dataset;
+      localStorage.setItem("name", name);
+
+      window.location.href = "slide.html";
+    });
+  });
+}
+navigateToASlide();
+
+function startSlideshow() {
+  const startSlideshow = document.querySelector(".js-start-slideshow");
+
+  startSlideshow.addEventListener("click", () => {
+    localStorage.setItem("name", paintings[0].name);
+  });
+}
+startSlideshow();
